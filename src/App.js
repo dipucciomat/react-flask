151fb8072a89c40dfd9319b780574deb88a98823
dipuccio <dipuccio@umich.edu>
2020-10-26 19:37:40 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  //invoked when this page is rendered
+  //have to set to empty list so it doesn't get called recursively
+  //this way only calls oce 
+  useEffect(() => {
+    //fetch api call to time
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  useEffect(() => {
+    //fetch api call to time
+    fetch('/parse').then(res => res.json()).then(data => {
+      console.log(data);
+    });
+  }, []);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        ... no changes in this part ...
+
+        <p>The current time is {currentTime}.</p>
       </header>
+
+      <form action="/parse" method="post" encType="multipart/form-data">
+      <input type="file" name="file"></input>
+      <input type= "submit" name = "update" value="submit"/>   
+      </form>
     </div>
   );
 }
