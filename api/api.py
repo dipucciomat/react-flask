e1127172131d28322c267661524d1cd9e5dd32df
dipuccio <dipuccio@umich.edu>
2020-10-26 19:33:44 -0400

    first commit

--- api/api.py
+++ api/api.py
@@ -1,8 +1,23 @@
 import time
+import requests
 from flask import Flask
+from flask import request
+from flask import jsonify
+
+
 
 app = Flask(__name__)
+
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/parse', methods=['POST'])
+def parse_resume():
+    print(request.form)
+    resume = request.files['file']
+    response = requests.post('https://jobs.lever.co/parseResume', files=dict(resume=resume))
+
+    return response.json()
